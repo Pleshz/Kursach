8769c82d0761742aff65034918039e3c755e45c2
Pleshz <pleskovd96@gmail.com>
2026-04-30 17:40:23 +0500

    Добавил гугл аналитику

--- frontend/src/pages/client/Index.vue
+++ frontend/src/pages/client/Index.vue
@@ -1,5 +1,16 @@
 ﻿<template>
   <body class="min-h-screen bg-gray-50 text-gray-800">
+    <script type="text/javascript">
+      (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+      })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108989745', 'ym');
+
+      ym(108989745, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/108989745" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <header class="border-b border-solid border-gray-200 bg-white">
       <div class="px-6 max-[767px]:px-4 max-[479px]:px-3 py-4 flex flex-wrap items-center justify-between gap-4 max-w-6xl mx-auto">
         <router-link to="/" class="text-2xl font-bold text-gray-900 no-underline">
